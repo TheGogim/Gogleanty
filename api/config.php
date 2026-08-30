52b51f7a17353b8d8cdf8b25a304089397d2a00c
TheGogim <danilogogi@gmail.com>
2026-08-30 17:51:43 -0500

    Seguridad: bloquear uploads, cerrar CORS y proteger archivos sensibles

--- api/config.php
+++ api/config.php
@@ -99,9 +99,6 @@ function getDBConnection()
 
 // Headers para API
 header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 // Manejar preflight requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
